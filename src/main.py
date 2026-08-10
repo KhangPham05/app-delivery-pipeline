60275a873ff80c5afc975a73d3cb419cf16f5288
KhangPham05 <khangpham05tv@gmail.com>
2026-08-09 22:46:40 -0700

    Add /healthz endpoint and __main__ runner

--- src/main.py
+++ src/main.py
@@ -1,6 +1,16 @@
+import uvicorn
 from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/")
 async def read_root():
     return {"Hello": "World"}
+
+@app.get("/healthz")
+async def read_health():
+    return {"status": "healthy"}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
+    
